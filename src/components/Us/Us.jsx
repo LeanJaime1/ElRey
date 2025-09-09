92df21000e68fc3735076b1ae5adbba82ba7ad3d
Leandro Jaime <leandrojaime17@gmail.com>
2025-09-09 19:07:00 -0300

    textos

--- src/components/Us/Us.jsx
+++ src/components/Us/Us.jsx
@@ -10,19 +10,19 @@ const Us = () => {
       <main className="us-content">
         <h1>Quiénes Somos</h1>
         <p>
-          En El Rey de las Ojotas contamos con más de 10 años de experiencia en la fabricación de ojotas lisas y personalizadas de alta calidad. Nos especializamos en crear productos únicos que combinan confort, estilo y funcionalidad, pensados para empresas, hoteles, eventos corporativos y sociales.
+          En El Rey de las Ojotas contamos con más de 10 años de experiencia en la fabricación de ojotas lisas, ojotas personalizadas, ojotas para sublimar, ojotas descartables y pantuflas personalizadas, de alta calidad, ideales para empresas, hoteles, eventos corporativos y sociales. Cada par es diseñado a medida, con el logo o identidad visual de tu marca, ofreciendo un producto único que combina confort, estilo y promoción.
         </p>
         <p>
-          Diseñamos cada par a medida, incorporando el logo o identidad visual de tu marca para que cada paso deje huella. Nuestro equipo de diseño gráfico trabaja sin cargo adicional para ofrecer propuestas visuales que se adapten exactamente a lo que imaginás.
+          Ofrecemos diseños sin cargo, adaptándonos a tus necesidades para que el resultado sea exactamente lo que imaginás.
         </p>
         <p>
-          Ofrecemos atención personalizada, cumplimiento en los tiempos de entrega y pedidos por mayor, garantizando una solución profesional y a medida para cada cliente.
+         Ya sea para regalos empresariales, souvenirs de eventos, kits de bienvenida en hoteles o acciones de marketing, nuestras ojotas personalizadas son una forma original y funcional de dejar huella.
         </p>
         <p>
-          Estamos en Buenos Aires y realizamos envíos a todo el país.
+           Más de 10 años de experiencia, personalización completa con logo o diseño, diseño gráfico sin costo, pedidos por mayor, atención personalizada y cumplimiento en tiempos de entrega, fabricación naciona, estamos en buenos aires, con envios a todo el país. 
         </p>
         <p>
-          Convertí cada paso en una experiencia de marca.
+         Convertí cada paso en una experiencia de marca. ¡Consultanos y hacé la diferencia con tus propias ojotas personalizadas!
         </p>
         <p>
           ¡Consultanos y hacé la diferencia con tus propias ojotas personalizadas!
